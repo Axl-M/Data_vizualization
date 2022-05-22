8d12db643d307b90a4cd4bc15a2411ae4d9537b8
Alexander M <axlmalyasov@gmail.com>
2022-05-22 15:40:00 +1000

    rw_visual.py mark begining by red dot and end - green

--- rw_visual.py
+++ rw_visual.py
@@ -8,6 +8,9 @@ while True:
     rw.fill_walk()
     point_numbers = list(range(rw.num_points))  # размер равен количеству точек в блуждании
     plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
+    # Выделение первой и последней точек.
+    plt.scatter(0, 0, c='green', edgecolors='none', s=100)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
     plt.show()
 
     keep_running = input("Make another walk? (y/n): ")
