5a6b5cf2b98c2c03a5afae2a1ffc53389ce513ce
Alexander M <axlmalyasov@gmail.com>
2022-05-22 14:14:01 +1000

    color red. numbers y axis wrong!

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
-plt.scatter(x_values, y_values, s=100)
+# x_values = [1, 2, 3, 4, 5]
+x_values = list(range(1, 1001))
+# y_values = [1, 4, 9, 16, 25]
+y_values = [x ** 2 for x in x_values]
+plt.scatter(x_values, y_values, c='red', edgecolors='none', s=40)
 
 # Назначение заголовка диаграммы и меток осей.
 plt.title("Square Numbers", fontsize=24)
@@ -11,5 +13,8 @@ plt.ylabel("Square of value", fontsize=14)
 
 # Назначение размера шрифта делений на осях.
 plt.tick_params(axis='both', which='major', labelsize=14)
+
+# Назначение диапазона для каждой оси.
+plt.axis([0, 1100, 0, 1100000])
 
 plt.show()
