13686cf1e54c1a751442e9a5df81e6e9286e6e47
Alexander M <axlmalyasov@gmail.com>
2022-05-22 15:58:41 +1000

    class Die created

--- rw_visual.py
+++ rw_visual.py
@@ -4,13 +4,20 @@ from random_walk import RandomWalk
 # Новые блуждания строятся до тех пор, пока программа остается активной.
 while True:
     # Построение случайного блуждания и нанесение точек на диаграмму.
-    rw = RandomWalk()
+    rw = RandomWalk(50000)
     rw.fill_walk()
+    # Назначение размера области просмотра.
+    plt.figure(figsize=(10, 6))     # размер в дюймах
+    # plt.figure(dpi=128, figsize=(10, 6))
     point_numbers = list(range(rw.num_points))  # размер равен количеству точек в блуждании
-    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=1)
     # Выделение первой и последней точек.
     plt.scatter(0, 0, c='green', edgecolors='none', s=100)
     plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+    # Удаление осей.
+    # plt.axes().get_xaxis().set_visible(False)
+    # plt.axes().get_yaxis().set_visible(False)
+
     plt.show()
 
     keep_running = input("Make another walk? (y/n): ")
