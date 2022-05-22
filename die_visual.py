1b1c48b3f1dcb1fcc97039a57d2a69a4765fbc79
Alexander M <axlmalyasov@gmail.com>
2022-05-22 19:58:17 +1000

    analize results

--- die_visual.py
+++ die_visual.py
@@ -5,8 +5,15 @@ die = Die()
 
 # Моделирование серии бросков с сохранением результатов в списке.
 results = []
-for roll_number in range(100):
+for roll_number in range(1000):
     result = die.roll()
     results.append(result)
 
-print(results)
+# Анализ результатов.
+frequencies = []
+for value in range(1, die.num_sides + 1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+print(frequencies)
+
