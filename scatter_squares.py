0e481fe7fdf43dc6e9f9a3a77046230b7724bfcd
Alexander M <axlmalyasov@gmail.com>
2022-05-22 14:27:46 +1000

    color map

--- scatter_squares.py
+++ scatter_squares.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 x_values = list(range(1, 1001))
 # y_values = [1, 4, 9, 16, 25]
 y_values = [x ** 2 for x in x_values]
-plt.scatter(x_values, y_values, c='red', edgecolors='none', s=40)
+# plt.scatter(x_values, y_values, c=(0, 0, 0.8), edgecolors='none', s=40)
+plt.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, edgecolors='none', s=40)
 
 # Назначение заголовка диаграммы и меток осей.
 plt.title("Square Numbers", fontsize=24)
