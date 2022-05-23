1356d944323d288d6b4a9eb7a56d981f06ab7f5b
Alexander M <axlmalyasov@gmail.com>
2022-05-23 18:17:42 +1000

    API working with response dict

--- python_repos.py
+++ python_repos.py
@@ -23,5 +23,24 @@ response_dict = r.json()
 print(response_dict.keys())     # ['total_count', 'incomplete_results', 'items']
 # total_count - к-во найденных ответов на запрос
 # incomplete_results - false если вызов завершился без ошибки
-# items - список словарей со всеми данными
+# items - список со словарями, каждый из которых содержит данные об одном репозитории Python
+print("Total repositories:", response_dict['total_count'])  # общее количество репозиториев Python в GitHub
 
+# Анализ информации о репозиториях.
+repo_dicts = response_dict['items'] # список со словарями, каждый из которых содержит данные об одном репозитории Python
+print("Repositories returned:", len(repo_dicts))    # 30
+
+# Анализ первого репозитория.
+repo_dict = repo_dicts[0]
+# print("\nKeys:", len(repo_dict))    # 78
+# for key in sorted(repo_dict.keys()):
+#     print(key)
+
+print("\nSelected information about first repository:")
+print('Name:', repo_dict['name'])
+print('Owner:', repo_dict['owner']['login'])
+print('Stars:', repo_dict['stargazers_count'])
+print('Repository:', repo_dict['html_url'])
+print('Created:', repo_dict['created_at'])
+print('Updated:', repo_dict['updated_at'])
+print('Description:', repo_dict['description'])
