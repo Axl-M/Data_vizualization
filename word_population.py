a0c127bc9eafee85ddf97212be9235012df3b319
Alexander M <axlmalyasov@gmail.com>
2022-05-23 12:24:02 +1000

    JSON got 2-digit country code for the given country

--- word_population.py
+++ word_population.py
@@ -1,4 +1,5 @@
 import json
+from country_codes import get_country_code
 
 # Список заполняется данными.
 filename = 'population_data.json'
@@ -10,4 +11,11 @@ for pop_dict in pop_data:       # перебор словарей
     if pop_dict['Year'] == '2010':
         country_name = pop_dict['Country Name']
         population = int(float(pop_dict['Value']))  # float т.к. встречаются дробные значения (может интерполировали)
-        print(country_name + ': ' + str(population))
+        # print(country_name + ': ' + str(population))
+
+        # получаем для заданной страны ее код Pygal, состоящий из 2 букв. (т.к. в JSON 3-х буквенные коды)
+        code = get_country_code(country_name)
+        if code:
+            print(code + ': ' + str(population))
+        else:                                   # Если код недоступен
+            print('ERROR - ' + country_name)
