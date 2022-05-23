37fa4c08e373096517baeda6be95ab7348d42463
Alexander M <axlmalyasov@gmail.com>
2022-05-23 11:51:45 +1000

    JSON got population info only for 2010 year

--- word_population.py
+++ word_population.py
@@ -5,4 +5,9 @@ filename = 'population_data.json'
 with open(filename) as f:
     pop_data = json.load(f)
 
-print(pop_data)
+# Вывод населения каждой страны за 2010 год.
+for pop_dict in pop_data:       # перебор словарей
+    if pop_dict['Year'] == '2010':
+        country_name = pop_dict['Country Name']
+        population = pop_dict['Value']
+        print(country_name + ': ' + population)
