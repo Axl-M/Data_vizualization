3193a05c958235a0aefe4e67477b53a4b454b560
Alexander M <axlmalyasov@gmail.com>
2022-05-23 14:52:52 +1000

    JSON plot full world map

--- world_population.py
+++ world_population.py
@@ -1,4 +1,5 @@
 import json
+import pygal
 from country_codes import get_country_code
 
 # Список заполняется данными.
@@ -6,7 +7,9 @@ filename = 'population_data.json'
 with open(filename) as f:
     pop_data = json.load(f)
 
+# Построение словаря с данными численности населения.
 # Вывод населения каждой страны за 2010 год.
+cc_populations = {}
 for pop_dict in pop_data:       # перебор словарей
     if pop_dict['Year'] == '2010':
         country_name = pop_dict['Country Name']
@@ -16,6 +19,14 @@ for pop_dict in pop_data:       # перебор словарей
         # получаем для заданной страны ее код Pygal, состоящий из 2 букв. (т.к. в JSON 3-х буквенные коды)
         code = get_country_code(country_name)
         if code:
-            print(code + ': ' + str(population))
-        else:                                   # Если код недоступен
-            print('ERROR - ' + country_name)
+            # print(code + ': ' + str(population))
+            cc_populations[code] = population
+        # else:                                   # Если код недоступен
+        #     print('ERROR - ' + country_name)
+
+
+
+wm = pygal.maps.world.World()
+wm.title = 'Мировая популяция в 2010, по странам'
+wm.add('2010', cc_populations)
+wm.render_to_file('world_population.svg')
