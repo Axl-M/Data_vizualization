eec9ee698736c988180300fa69a60a2d1e7c2b02
Alexander M <axlmalyasov@gmail.com>
2022-05-23 19:44:14 +1000

    API, visualization Most-Starred Python Projects on GitHub

--- python_repos.py
+++ python_repos.py
@@ -10,9 +10,11 @@ https://api.github.com/search/repositories?q=language:python&sort=stars
 Завершающая часть, &sort=stars, сортирует проекты по количеству присвоенных им звезд.
 """
 import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
 # Создание вызова API и сохранение ответа.
-url ='https://api.github.com/search/repositories?q=language:python&sort=stars'
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 r = requests.get(url)
 print('Status code: ', r.status_code)   # Код 200 — признак успешного ответа.
 
@@ -20,20 +22,32 @@ print('Status code: ', r.status_code)   # Код 200 — признак успе
 response_dict = r.json()
 
 # Обработка результатов.
-print(response_dict.keys())     # ['total_count', 'incomplete_results', 'items']
+# print(response_dict.keys())     # ['total_count', 'incomplete_results', 'items']
 # total_count - к-во найденных ответов на запрос
 # incomplete_results - false если вызов завершился без ошибки
 # items - список со словарями, каждый из которых содержит данные об одном репозитории Python
 print("Total repositories:", response_dict['total_count'])  # общее количество репозиториев Python в GitHub
 
 # Анализ информации о репозиториях.
-repo_dicts = response_dict['items'] # список со словарями, каждый из которых содержит данные об одном репозитории Python
-print("Repositories returned:", len(repo_dicts))    # 30
+repo_dicts = response_dict['items']  # список словарей, каждый из которых содержит данные об одном репозитории Python
+# print("Repositories returned:", len(repo_dicts))    # 30
 
-print("\nSelected information about each repository:")
+# print("\nSelected information about each repository:")
+names, stars = [], []
 for repo_dict in repo_dicts:
-    print('\nName:', repo_dict['name'])
-    print('Owner:', repo_dict['owner']['login'])
-    print('Stars:', repo_dict['stargazers_count'])
-    print('Repository:', repo_dict['html_url'])
-    print('Description:', repo_dict['description'])
+    names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
+    # print('\nName:', repo_dict['name'])
+    # print('Owner:', repo_dict['owner']['login'])
+    # print('Stars:', repo_dict['stargazers_count'])
+    # print('Repository:', repo_dict['html_url'])
+    # print('Description:', repo_dict['description'])
+
+# построение визуализации
+my_style = LS('#333366', base_style=LCS)
+chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
+chart.title = 'Most-Starred Python Projects on GitHub'
+chart.x_labels = names
+chart.add('', stars)
+chart.render_to_file('python_repos.svg')
